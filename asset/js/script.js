fa07afc572334a4548d52d2f97cae17a78fccc16
Md. Anower Hossan <anowerhossan1988@gmail.com>
2023-12-24 17:45:06 +0600

    urlwise seperated page

---
+++ asset/js/script.js
@@ -0,0 +1,12 @@
+// Hospital list search bar icon view
+// $(".h_search_bar").focus(function () {
+//   console.log($("#h_top_search_icon"));
+//   $("#h_top_search_icon").classList.toggle("display_block");
+// });
+
+// let search_bar = document.querySelector(".h_search_bar");
+// console.log(search_bar)
+// search_bar.addEventListener('focus', function(){
+//     let search_icon = document.getElementById("#h_top_search_icon");
+//     search_icon.classList.add("display_block");
+// })
